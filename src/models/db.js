ddec407efc81bed043ecef72855bfbdf1e1719f8
Myamiki <146244292+Myamiki@users.noreply.github.com>
2026-06-14 09:44:09 +0200

    Remove fallback DB connection string option

--- src/models/db.js
+++ src/models/db.js
@@ -12,12 +12,11 @@ import { Pool } from 'pg';
  * postgresql://username:password@host:port/database
  */
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL || process.env.DB_URL,
+    connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
     }
 });
-
 /**
  * Common SSL Issue:
  *
